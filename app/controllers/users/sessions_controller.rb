bff9accf24854b2e904632aa153cf4ea3cfa4b07
Tom Hall <tomhall.main@gmail.com>
2025-04-28 22:00:57 -0400

    Fix sessions controller for main application case

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,13 +1,43 @@
 class Users::SessionsController < Devise::SessionsController
-  respond_to :json
+  before_action :set_request_format
+
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    
+    respond_to do |format|
+      format.html { super }
+      format.json { render json: { error: "You need to sign in or sign up before continuing." }, status: :unauthorized }
+    end
+  end
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    
+    respond_to do |format|
+      format.json { render json: { message: "Signed in successfully.", user: resource }, status: :ok }
+      format.html { respond_with resource, location: after_sign_in_path_for(resource) }
+    end
+  end
+
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    
+    respond_to do |format|
+      format.json { render json: { message: "Signed out successfully." }, status: :ok }
+      format.html { respond_to_on_destroy }
+    end
+  end
 
   private
 
-  def respond_with(resource, _opts = {})
-    render json: { message: "Signed in successfully.", user: resource }, status: :ok
-  end
-
-  def respond_to_on_destroy
-    render json: { message: "Signed out successfully." }, status: :ok
+  def set_request_format
+    request.format = :html if request.format == Mime[:json] && request.headers['HTTP_ACCEPT'].include?('text/html')
   end
 end 
